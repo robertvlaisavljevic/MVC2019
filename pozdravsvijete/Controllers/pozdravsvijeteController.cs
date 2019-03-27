4b8cfd92f293c9b8b51070ffb5006cb7f02fb104
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-27 18:46:04 +0100

    aha

---
+++ pozdravsvijete/Controllers/pozdravsvijeteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pozdravsvijete.Controllers
+{
+    public class pozdravsvijeteController : Controller
+    {
+        // GET: pozdravsvijete
+        public ActionResult Index()
+        {
+            string model = "Pozdrav svijete";
+            return View((object(model);
+        }
+    }
+}
