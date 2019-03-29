c1b5f8ff0b7c38a4afd93b26d097c62b58d3ca9e
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-29 17:33:25 +0100

    4.1.1 jednostavni model

---
+++ mvc_2_1/Controllers/GeneratorIzlazaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_2_1.Controllers
+{
+    public class GeneratorIzlazaController : Controller
+    {
+        // GET: GeneratorIzlaza
+        public ActionResult RedirectNaMetodu(string id)
+        {
+            if (id=="pozdrav")
+            {
+                return RedirectToAction("pozdravsvijete", "Index");
+            }
+            return View();
+        }
+
+
+    }
+}
