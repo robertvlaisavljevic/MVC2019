dc4a8899396500305dadea9178e2fd74815213e7
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-29 18:39:29 +0100

    modeli

---
+++ modeli/Controllers/OsobeController.cs
@@ -0,0 +1,24 @@
+﻿using modeli.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modeli.Controllers
+{
+    public class OsobeController : Controller
+    {
+        // GET: Osobe
+        public ViewResult PopuniOsobu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult PrikaziOsobu (Osoba osoba)
+        {
+            return View(osoba);
+        }
+    }
+}
