5380dc8db0ceaf39e90d1aa3f36ff290d0e608e0
robert vlaisavljevic <korisnik92@gmail.com>
2019-04-12 18:28:07 +0200

    kjk

---
+++ WebShop/Controllers/CartController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop.Models;
+using System.Data.Entity;
+
+namespace WebShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private WebShopEntities db = new WebShopEntities();
+        public static List<Proizvodi> lstProizvodi = new List<Proizvodi>();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            if (Session["Cart"] !=null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+            return View(lstProizvodi);
+        }
+        // GET: Cart/AddToCart/{id}
+        public ActionResult AddToCart(int id)
+        {
+
+
+            /*
+            if (Session["Cart"] != null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+            */
+            
+            
+            Proizvodi proizvodi = db.Proizvodis.Find(id);
+            lstProizvodi.Add(proizvodi);
+            Session["Cart"] = lstProizvodi;
+
+            if (proizvodi==null)
+            {
+                return HttpNotFound("greska");
+            }
+            var proi = db.Proizvodis.Include(p => p.MjereProizvoda);
+            return RedirectToAction(actionName: "Index", controllerName: "WebShop", routeValues: proi.ToList());
+        }
+    }
+}
