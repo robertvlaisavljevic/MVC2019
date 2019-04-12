fea39eef8e111b78fe9d13b2f04caa95593d4324
robert vlaisavljevic <korisnik92@gmail.com>
2019-04-12 15:55:59 +0200

    da

--- Adresar/Controllers/KontaktController.cs
+++ Adresar/Controllers/KontaktController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,5 +21,101 @@ namespace Adresar.Controllers
                                              select k).ToList();
             return View(aktivniKontakti);
         }
+
+        // GET: Kontakt/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Kontakt/Create
+        [HttpPost]
+        public ActionResult Create(Kontakt kontakt)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Kontakti.Add(kontakt);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kontakt);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Kontakt kontakt)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Kontakti.Add(kontakt);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                
+            }
+            return View(kontakt);
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            {
+                Kontakt kontakt = _db.Kontakti.Find(id);
+                _db.Kontakti.Remove(kontakt);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                
+            }
+            
+        }
+
+
     }
 }
